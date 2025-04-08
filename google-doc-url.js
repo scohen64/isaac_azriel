d8a474546bb2f4934addcb73490eada8bbb42548
alonlabock <axl4336@miami.edu>
2025-04-08 13:10:49 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTGTot51WMhXtnz9aQwQbuW7C-KMYiVycxIMksch5QfAIRZLmfsCr7PeTB87ExJISzvqvSm7DPOEAGC/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/10-iE5kNgS9F7ClSP81yujO1NK14ev-jfsFLnBRGFvuQ/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
